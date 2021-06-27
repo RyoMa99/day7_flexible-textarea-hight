d1a7b34bfd65315632cae33b875a21377cb2e43d
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-06-27 14:48:34 +0900

    fin

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 [flexible textarea hight](https://reffect.co.jp/vue/textarea_resize_vue_js)
 
 ## 学んだこと
-
-
+- [Element.scrollHeight は読み取り専用のプロパティで、あふれて画面上に表示されない部分を含めた、要素の中身の幅の寸法](https://developer.mozilla.org/ja/docs/Web/API/Element/scrollHeight)
+- `@Watch('tweet', { immediate: true })`で初回もwatchを走らせる
+- [`Vue.$nextTick` DOM更新後にコールバックを実行する](https://jp.vuejs.org/v2/api/index.html)
 ## Project setup
 ```
 yarn install
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>init</div>
+  <div>
+    <textarea
+      ref="area"
+      v-model="tweet"
+      :style="styles"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 
 @Component({})
 export default class extends Vue {
+  tweet = ''
+  height =''
+  $refs!: {
+    area: HTMLInputElement
+  }
 
+  @Watch('tweet', { immediate: true })
+  onChangeWatch (): void {
+    this.resize()
+  }
+
+  get styles (): { height: string} {
+    return {
+      height: this.height
+    }
+  }
+
+  resize (): void {
+    // 文字列が横に長くなるだけでもtextareaが縦に長くなるので不適切
+    // this.height = this.$refs.area.scrollHeight + 'px'
+    this.height = 'auto'
+    this.$nextTick(() => {
+      this.height = this.$refs.area.scrollHeight + 'px'
+    })
+  }
 }
 </script>
 
